dd6c346164e97f0dbe7c036765cadb72b8080f68
softgenai <admin@softgen.ai>
2026-01-31 22:57:00 +0000

    Updates

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -12,4 +12,4 @@ export type Enums<T extends keyof Database['public']['Enums']> = Database['publi
 export type Functions<T extends keyof Database['public']['Functions']> = Database['public']['Functions'][T];
 
 // Project reference: nyylghvqdfggyfefeqtd
-// Last updated: 2026-01-31T22:50:44.669168
+// Last updated: 2026-01-31T22:56:44.297888
